9d268076904c29ec1540ede570f4291cc4d4be3b
Nitish Saha <nitisaha@cisco.com>
2020-06-13 14:15:47 +0530

    Created models

---
+++ src/api/models/github/create_repo.go
@@ -0,0 +1,45 @@
+package github
+
+/*
+{
+  "name": "Hello-World",
+  "description": "This is your first repository",
+  "homepage": "https://github.com",
+  "private": false,
+  "has_issues": true,
+  "has_projects": true,
+  "has_wiki": true
+}
+
+*/
+
+type CreateRepoRequest struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Homepage    string `json:"homepage"`
+	Private     bool   `json:"private"`
+	HasIssues   bool   `json:"has_issues"`
+	HasProjects bool   `json:"has_projects"`
+	HasWiki     bool   `json:"has_wiki"`
+}
+
+type CreateRepoResponse struct {
+	Id          string          `json:"id"`
+	Name        string          `json:"name"`
+	Fullname    string          `json:"full_name"`
+	Owner       RepoOwner       `json:"owner"`
+	Permissions RepoPermissions `json:"permissions"`
+}
+
+type RepoOwner struct {
+	Id      int64  `json:"id"`
+	Login   string `json:"login"`
+	Url     string `json:"url"`
+	HtmlUrl string `json:"html_url"`
+}
+
+type RepoPermissions struct {
+	IsAdmin bool `json:"admin"`
+	HasPull bool `json:"pull"`
+	HasPush bool `json:"push"`
+}
---
+++ src/api/models/github/create_repo_test.go
@@ -0,0 +1,36 @@
+package github
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateRepoRequestAsJson(t *testing.T) {
+	request := CreateRepoRequest{
+		Name:        "nitish-go-test-repo",
+		Description: "A golang test repo",
+		Private:     true,
+		HasIssues:   true,
+		HasProjects: true,
+		HasWiki:     true,
+	}
+
+	// Marshal takes an input interface and attempts to create a valid JSON string.
+	bytes, err := json.Marshal(request)
+	assert.Nil(t, err)
+	assert.NotNil(t, `{"name":"nitish-go-test-repo","description":"A golang test repo","homepage":"","private":true,"has_issues":true,"has_projects":true,"has_wiki":true}`, string(bytes))
+	//assert.EqualValues(t, )
+
+	fmt.Println(string(bytes))
+
+	// Convert the previous json and load it in 'target' struct variable.
+	var target CreateRepoRequest
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, target.Name, request.Name)
+	assert.EqualValues(t, target.HasIssues, request.HasIssues)
+}
